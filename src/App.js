7c1ea52d46f0ce418f6dea767194bbb75daaaf95
Neto Concon <concon.neto2@gmail.com>
2019-12-25 16:58:55 -0300

    add states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 29}
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('was clicked');
+    // this.state.persons[0].name = "Maximilian"; DO NOT DO THIS
+    this.setState({ persons: [
+      { name: 'Maximilan', age: 29},
+      { name: 'Manuela', age: 30}
+    ]})
+  };
+  
   render() {
     return (
       <div className="App">
         <h1>I am React App</h1>
         <p>This is really working</p>
+        <button onClick = {this.switchNameHandler} >Switch</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const person = (props) => {
+  return (
+    <div>
+      <p> I am a {props.name} and I am {props.age} </p>
+      <p> {props.children}</p>
+    </div>
+  )
+};
+
+export default person;
